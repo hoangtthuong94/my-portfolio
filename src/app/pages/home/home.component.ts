4ca416dc033ca09015622e0de216a0ff48e296e9
thuhuong.hoangthi <thuhuong.hoangthi@officience.com>
2019-02-18 17:02:25 +0700

    init project

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,41 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Typed from 'typed.js';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  @ViewChild('typedStrings') typedStrings: ElementRef;
+  @ViewChild('typed') typed: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+    /*const typed = new Typed(this.typedStrings.nativeElement, {
+      strings: ['First sentence.', 'Second sentence.'],
+    });*/
+
+   /* const typed = new Typed(this.typedStrings.nativeElement, {
+      strings: ['Hello, I\'m Huong', 'How are you?'],
+      typeSpeed: 100,
+      backSpeed: 0,
+      loop: true
+    });*/
+
+    const typed = new Typed(this.typed.nativeElement, {
+      stringsElement: this.typedStrings.nativeElement,
+      typeSpeed: 100,
+      backSpeed: 0,
+      backDelay: 500,
+      startDelay: 1000,
+      loop: true
+    });
+  }
+
+}
